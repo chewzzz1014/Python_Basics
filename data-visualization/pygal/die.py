b247984c44d22821dfe105cd060fe580d80bcb49
chewzzz <chewziqing@gmail.com>
2022-08-22 15:33:12 +0800

    Move data-visulization dir to new repo

--- data-visualization/pygal/die.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-class Die():
-    def __init__(self, num_sides=6):
-        self.num_sides = num_sides
-
-    def roll(self):
-        # any number x such that 1<= x <= num_sides
-        return randint(1, self.num_sides)
