477bed1eca77c40a4b115b112c039a71be321111
chewzzz <chewziqing@gmail.com>
2023-10-23 23:29:33 +0800

    start moocfi python course

---
+++ MoocFi_Courses/part1/1_getting-started.py
@@ -0,0 +1,20 @@
+print('Hello world')
+print(':-)')
+
+print('Juhani')
+print('Tuomas')
+print('Aapo')
+print('Simeoni')
+print('Lauri')
+print('Timo')
+print('Eero')
+
+print('Row, row, row your boat,')
+print('Gently down the stream.')
+print('Merrily, merrily, merrily, merrily,')
+print('Life is but a dream.')
+
+# num of minutes in a year
+print(60 * 24 * 365)
+
+print('print("Hello there!")')
---
+++ MoocFi_Courses/part1/2_user-input.py
@@ -0,0 +1,24 @@
+name = input('What is your name?')
+print(name)
+print(name)
+
+name = input('What is your name')
+print('!' + name + '!' + name + '!')
+
+
+fname = input('Given name:')
+lname = input('Family name:')
+street = input('City and postal code:')
+city_postal = input('City and postal code')
+print(fname + ' ' + lname)
+print(street)
+print(city_postal)
+
+part1 = input("The 1st part: ")
+part2 = input("The 2nd part: ")
+part3 = input("The 3rd part: ")
+print(part1 + part2 + part3)
+
+story_name = input('Please type in a name')
+story_year = input('Please type in a year')
+print(story_name + ' is a valiant knight, born in the year ' + story_year + '. One morning ' + story_name + ' woke up to an awful racket: a dragon was approaching the village. Only ' + story_name + ' could save the village\'s residents.')
