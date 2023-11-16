ed9fc9b030e07b769a655e21bdcc8f1c5e2af300
chewzzz <chewziqing@gmail.com>
2023-11-16 22:48:36 +0800

    add 2 more exercises

--- MoocFi_Courses/part6/1_read_file.py
+++ MoocFi_Courses/part6/1_read_file.py
@@ -45,3 +45,97 @@ def row_sums():
             result.append(sum(numbers))
     return result
 
+if True:
+    student_info = input("Student information: ")
+    exercise_data = input("Exercises completed: ")
+else:
+    # now this is the False branch, and is never executed
+    student_info = "students1.csv"
+    exercise_data = "exercises1.csv"
+students = {}
+grades = {}
+with open(student_info) as rf:
+    for line in rf:
+        parsed_values = line.replace('\n', '').split(';')
+        if parsed_values[0] == 'id':
+            continue
+        else:
+            students[parsed_values[0]] = parsed_values[1] + ' ' + parsed_values[2]
+
+with open(exercise_data) as rf:
+    for line in rf:
+        parsed_values = line.replace('\n', '').split(';')
+        if parsed_values[0] == 'id':
+            continue
+        else:
+            student_id = parsed_values[0]
+            grades[student_id] = []
+            for i in range(1, len(parsed_values)):
+                grades[student_id].append(int(parsed_values[i]))
+for id, name in students.items():
+    print(f'{name} {sum(grades[id])}')
+
+import math
+if True:
+    student_info = input("Student information: ")
+    exercise_data = input("Exercises completed: ")
+    exam_data = input("Exam points: ")
+else:
+    # now this is the False branch, and is never executed
+    student_info = "students1.csv"
+    exercise_data = "exercises1.csv"
+    exam_data = 'exam_points1.csv'
+students = {}
+points = {}
+with open(student_info) as rf:
+    for line in rf:
+        parsed_values = line.replace('\n', '').split(';')
+        if parsed_values[0] == 'id':
+            continue
+        else:
+            students[parsed_values[0]] = parsed_values[1] + ' ' + parsed_values[2]
+            points[parsed_values[0]] = []
+# read exercise point
+with open(exercise_data) as rf:
+    for line in rf:
+        parsed_values = line.replace('\n', '').split(';')
+        if parsed_values[0] == 'id':
+            continue
+        else:
+            student_id = parsed_values[0]
+            total_completion = 0
+            total_points = 0
+            for i in range(1, len(parsed_values)):
+                total_completion += int(parsed_values[i])
+
+            total_points = math.floor(total_completion / 40 * 10)
+            points[student_id].append(total_points)
+# read exam point
+with open(exam_data) as rf:
+    for line in rf:
+        parsed_values = line.replace('\n', '').split(';')
+        if parsed_values[0] == 'id':
+            continue
+        else:
+            student_id = parsed_values[0]
+            total_points = 0
+            for i in range(1, len(parsed_values)):
+                total_points += int(parsed_values[i])
+            points[student_id].append(total_points)
+# print(points)
+def get_grade(points):
+    if 0<=points<=14:
+        return 0
+    elif 15<=points<=17:
+        return 1
+    elif 18<=points<=20:
+        return 2
+    elif 21<=points<=23:
+        return 3
+    elif 24<=points<=27:
+        return 4
+    elif points>=28:
+        return 5
+for id, name in students.items():
+    print(f'{name} {get_grade(sum(points[id]))}')
+
