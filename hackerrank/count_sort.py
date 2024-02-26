5e75cd0a9e6e3011d360b86463ecb76b8994247b
chewzzz <chewziqing@gmail.com>
2024-02-26 17:59:46 +0800

    count sort application

--- hackerrank/count_sort.py
+++ hackerrank/count_sort.py
@@ -9,11 +9,28 @@ def countingSort(arr):
     return result
 
 
+def countSort(arr):
+    mid = len(arr) // 2
+    count_arr = [[] for _ in range(mid + 1)]
+
+    for idx, x in enumerate(arr):
+        if idx >= mid:
+            count_arr[int(x[0])].append(x[1])
+        else:
+            count_arr[int(x[0])].append('-')
+    print(' '.join([' '.join(x) for x in count_arr]).strip())
+
+
 if __name__ == '__main__':
     n = int(input('n: ').strip())
+    arr = []
+    for _ in range(n):
+        arr.append(input('input: ').rstrip().split())
+    countSort(arr)
 
-    arr = list(map(int, input('list of numbers: ').rstrip().split()))
 
-    result = countingSort(arr)
-
-    print(' '.join(map(str, result)))
+# if __name__ == '__main__':
+#     n = int(input('n: ').strip())
+#     arr = list(map(int, input('list of numbers: ').rstrip().split()))
+#     result = countingSort(arr)
+#     print(' '.join(map(str, result)))
